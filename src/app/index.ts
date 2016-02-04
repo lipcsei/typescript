5614b31ca0fca2bda88a39e220c4761926008567
Lipcsei Sandor <sandor.lipcsei@gmail.com>
2016-02-04 19:09:40 +0100

    init

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,3 +1,7 @@
 import * as _ from 'lodash';
+import * as firebase from 'firebase';
 
 console.log(_.chunk(['a', 'b', 'c', 'd'], 2));
+
+
+
--- tools/typings/tsd.d.ts
+++ tools/typings/tsd.d.ts
@@ -2,3 +2,4 @@
 /// <reference path="../../node_modules/definition-header/dist/index.d.ts" />
 /// <reference path="../../node_modules/immutable/dist/immutable.d.ts" />
 /// <reference path="../../node_modules/tslint/lib/tslint.d.ts" />
+/// <reference path="firebase-token-generator/firebase-token-generator.d.ts" />
